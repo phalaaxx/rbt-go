65456b90eef8bf16f88503be9fb535b9a707a3a3
Bozhin Zafirov <bozhin@sirius.local>
2017-04-03 11:16:06 +0300

    support multiple input configuration files in one run

--- main.go
+++ main.go
@@ -11,28 +11,30 @@ func main() {
 	flag.Parse()
 	// check if positional arguments are provided
 	if flag.NArg() != 1 {
-		fmt.Println("Usage: backup <file.json>")
+		fmt.Println("Usage: backup file.json [file1.json ...]")
 		return
 	}
-	// read and parse backup configuration file
-	rsync, err := ParseConfigFile(flag.Arg(0))
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	// make sure backup directories exist
-	if err := rsync.Init(); err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-	// perform backup
-	if allow, err := rsync.AllowBackup(); err != nil {
-		fmt.Println("AllowBackup():", err)
-		return
-	} else if allow {
-		if err := DoLock(rsync); err != nil {
-			fmt.Println("DoLock():", err)
-			return
+	for idx := 0; idx < flag.NArg(); idx++ {
+		// read and parse backup configuration file
+		rsync, err := ParseConfigFile(flag.Arg(idx))
+		if err != nil {
+			fmt.Printf("Error: %s: %v\n", flag.Arg(idx), err)
+			continue
+		}
+		// make sure backup directories exist
+		if err := rsync.Init(); err != nil {
+			fmt.Printf("Init(): %s: %v\n", rsync.Name, err)
+			continue
+		}
+		// perform backup
+		if allow, err := rsync.AllowBackup(); err != nil {
+			fmt.Printf("AllowBackup(): %s: %v\n", rsync.Name, err)
+			continue
+		} else if allow {
+			if err := DoLock(rsync); err != nil {
+				fmt.Printf("DoLock(): %s: %v\n", rsync.Name, err)
+				continue
+			}
 		}
 	}
 }
