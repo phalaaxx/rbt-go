c4566ccc0ed757f9d3d3e681d17d952456f9e01a
Bozhin Zafirov <bozhin@sirius.local>
2017-04-03 14:44:56 +0300

    use flags instead of positional arguments to provide configuration files

--- main.go
+++ main.go
@@ -3,22 +3,40 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 )
+
+// ConfigFiles defines a list of strings
+type ConfigFiles []string
+
+// String returns a representation of the object
+func (f *ConfigFiles) String() string {
+	return strings.Join(*f, ",")
+}
+
+// Set adds a new value to the list
+func (f *ConfigFiles) Set(value string) error {
+	*f = append(*f, value)
+	return nil
+}
 
 /* main program */
 func main() {
 	// parse command line flags
+	var files ConfigFiles
+	flag.Var(&files, "f", "Backup configuration file.")
 	flag.Parse()
-	// check if positional arguments are provided
-	if flag.NArg() != 1 {
-		fmt.Println("Usage: backup file.json [file1.json ...]")
+	// make sure there are file names provided
+	if len(files) == 0 {
+		fmt.Println("For usage use -h")
 		return
 	}
-	for idx := 0; idx < flag.NArg(); idx++ {
+	// run backup process for all provided arguments
+	for _, file := range files {
 		// read and parse backup configuration file
-		rsync, err := ParseConfigFile(flag.Arg(idx))
+		rsync, err := ParseConfigFile(file)
 		if err != nil {
-			fmt.Printf("Error: %s: %v\n", flag.Arg(idx), err)
+			fmt.Printf("Error: %s: %v\n", file, err)
 			continue
 		}
 		// make sure backup directories exist
